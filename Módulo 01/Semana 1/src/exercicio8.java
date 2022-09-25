aea5deffd5b27f74a4020d174e12582134a93d7e
Fabiane Zanoni <109667955+zanoniFab@users.noreply.github.com>
2022-09-24 21:23:24 -0300

    Nome do time corrigido.

--- Módulo 01/Semana 1/src/exercicio8.java
+++ Módulo 01/Semana 1/src/exercicio8.java
@@ -1,14 +1,13 @@
 public class exercicio8 {
     public static void main(String[] args) {
-        String nomeTime = "Gremião";
+        String nomeTime = "Tabajara F.C";
         int vitorias = 12;
         int derrotas = 2;
         int empates = 6;
         int partidas = vitorias + derrotas + empates;
         int pontos = (vitorias*3) + empates;
 //        System.out.println("O time " + nomeTime + " acumulou "+ pontos + " pontos após jogar " + partidas + " partidas.");
-//        %s substitui por string %d por numero inteiro %f por float
-        String fraseFinal = String.format("O time %s acumulou %d pontos após jogar %d partidas.", nomeTime,pontos,partidas);
-        System.out.println(fraseFinal);
+//        %s substitui por string %d por numero int %f por float
+        System.out.printf("O time %s acumulou %d pontos após jogar %d partidas.",nomeTime,pontos,partidas);
     }
 }
