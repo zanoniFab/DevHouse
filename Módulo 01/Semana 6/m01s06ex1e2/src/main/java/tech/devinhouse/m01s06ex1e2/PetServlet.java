4bc0eb00785f96f5b68939196ed462deed2ddb6c
zanonifab <fabianezanoni@gmail.com>
2022-10-21 21:17:58 -0300

    implementa exercicio 2 semana 6

--- Módulo 01/Semana 6/m01s06ex1e2/src/main/java/tech/devinhouse/m01s06ex1e2/PetServlet.java
+++ Módulo 01/Semana 6/m01s06ex1e2/src/main/java/tech/devinhouse/m01s06ex1e2/PetServlet.java
@@ -5,29 +5,80 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import tech.devinhouse.m01s06ex1e2.models.Pet;
+import tech.devinhouse.m01s06ex1e2.models.Tutor;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet(value="/PetServlet")
 public class PetServlet extends HttpServlet {
-//    private static List<Tutor> tutores = new ArrayList<>();
+    private List<Pet> pets = new ArrayList<>();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println("Get-Pet");
+        for (Pet pet : pets) {
+            resp.getWriter().println(pet);
+        }
+
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println("Post-Pet");
+        String nome = req.getParameter("nome");
+        String tipo = req.getParameter("tipo");
+        String raca = req.getParameter("raça");
+        Integer idade = Integer.parseInt(req.getParameter("idade"));
+        String alimentoPreferido = req.getParameter("alimentoPreferido");
+        Pet pet = Pet.builder()
+                .nome(nome)
+                .tipo(tipo)
+                .raca(raca)
+                .idade(idade)
+                .alimentoPreferido(alimentoPreferido)
+                .build();
+        pets.add(pet);
+        resp.getWriter().println("Sucesso!");
     }
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println("Put-Pet");
+        String nome = req.getParameter("nome");
+        Integer idade = Integer.parseInt(req.getParameter("idade"));
+        Integer id = Integer.parseInt(req.getParameter("id"));
+        String tipo = req.getParameter("tipo");
+        String raca = req.getParameter("raça");
+        String alimentoPreferido = req.getParameter("alimentoPreferido");
+
+        Pet pet = findById(id);
+        pet.setNome(nome);
+        pet.setIdade(idade);
+        pet.setTipo(tipo);
+        pet.setAlimentoPreferido(alimentoPreferido);
+        pet.setRaca(raca);
+        resp.getWriter().println("feito!");
     }
 
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println("Delete-Pet");
+        Boolean sucesso = true;
+        try {
+            Integer id = Integer.parseInt(req.getParameter("id"));
+            Pet pet = findById(id);
+            pets.remove(pet);
+        }catch (Exception e){
+            sucesso=false;
+        }
+        resp.getWriter().println(sucesso);
+    }
+
+    private Pet findById(Integer id) {
+        for (Pet pet : pets) {
+            if (id.equals(pet.getId())) {
+                return pet;
+            }
+        }
+        return null;
     }
 }
--- Módulo 01/Semana 6/m01s06ex1e2/src/main/java/tech/devinhouse/m01s06ex1e2/TutorServlet.java
+++ Módulo 01/Semana 6/m01s06ex1e2/src/main/java/tech/devinhouse/m01s06ex1e2/TutorServlet.java
@@ -5,31 +5,64 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
+import tech.devinhouse.m01s06ex1e2.models.Tutor;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 @WebServlet(value="/TutorServlet")
 public class TutorServlet extends HttpServlet {
-//    private static List<Tutor> tutores = new ArrayList<>();
+    private List<Tutor> tutores = new ArrayList<>();
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println("Get-Tutor");
+        for (Tutor tutor : tutores){
+            resp.getWriter().println(tutor);
+        }
+
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println("Post-Tutor");
+        String nome = req.getParameter("nome");
+        Integer idade = Integer.parseInt(req.getParameter("idade"));
+        Tutor tutor = Tutor.builder()
+                .nome(nome)
+                .idade(idade)
+                .build();
+        tutores.add(tutor);
+        resp.getWriter().println("Sucesso!");
     }
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println("Put-Tutor");
+        String nome = req.getParameter("nome");
+        Integer idade = Integer.parseInt(req.getParameter("idade"));
+        Integer id = Integer.parseInt(req.getParameter("id"));
+
+        Tutor tutor = findById(id);
+        tutor.setNome(nome);
+        tutor.setIdade(idade);
+        resp.getWriter().println("feito!");
     }
 
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println("Delete-Tutor");
+        Boolean sucesso = true;
+        try {
+            Integer id = Integer.parseInt(req.getParameter("id"));
+            Tutor tutor = findById(id);
+            tutores.remove(tutor);
+        }catch (Exception e){
+            sucesso=false;
+        }
+        resp.getWriter().println(sucesso);
+    }
+    private Tutor findById(Integer id){
+        for (Tutor tutor : tutores){
+            if (id.equals(tutor.getId())){
+                return tutor;
+            }
+        }
+        return null;
     }
 }
