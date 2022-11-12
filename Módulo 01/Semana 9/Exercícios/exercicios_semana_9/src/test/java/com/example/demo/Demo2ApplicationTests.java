ba65747e3fccf8e4ba296b19fd56078758578b06
zanonifab <fabianezanoni@gmail.com>
2022-11-12 12:51:45 -0300

    exercicio 1

--- Módulo 01/Semana 9/Exercícios/exercicios_semana_9/src/test/java/com/example/demo/Demo2ApplicationTests.java
+++ Módulo 01/Semana 9/Exercícios/exercicios_semana_9/src/test/java/com/example/demo/Demo2ApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.loja_util;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LojaUtilApplicationTests {
+class Demo2ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
